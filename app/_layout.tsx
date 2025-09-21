934132cc73dc8e2ec5cc75eb91e3fbf58d5ebdae
kirimuramari <lifemastert@gmail.com>
2025-09-21 17:23:41 +0900

    全体のレイアウトを設定

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
+import { View, StyleSheet } from "react-native";
 
 export default function RootLayout() {
   return <Slot />;
+  <View style={StyleSheet.container}>
+    <Stack screenOptions={{ headerShown: false }} />
+  </View>;
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f0f0f0",
+  },
+});

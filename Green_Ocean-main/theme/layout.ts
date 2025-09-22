05252c823f9db17b7a084a2bb6cc32d78e869759
kirimuramari <lifemastert@gmail.com>
2025-09-22 15:51:26 +0900

    Delete Green_Ocean-main directory

--- Green_Ocean-main/theme/layout.ts
+++
@@ -1,9 +0,0 @@
-//テーブルスマホ表示のデザイン崩れ防止
-import { StyleSheet as RNStyleSheet, StyleProp } from "react-native";
-
-export const flattenStyle = <T>(
-  base: StyleProp<T>,
-  extra?: StyleProp<T>
-): T => {
-  return RNStyleSheet.flatten([base, extra]) as T;
-};

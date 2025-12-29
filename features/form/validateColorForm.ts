42c5edefcc21a3e4c0e7392012eb26720cad6731
kirimuramari <lifemastert@gmail.com>
2025-12-29 13:57:06 +0900

    一時的に数値チェックをやめている

--- features/form/validateColorForm.ts
+++ features/form/validateColorForm.ts
@@ -4,9 +4,9 @@ export function validateColorForm(form: Colorform): string | null {
   if (!form.商品名.trim()) {
     return "商品名を入力してください";
   }
-  if (form.値段 !== null && isNaN(Number(form.値段))) {
-    return "値段は数値で入力してください";
-  }
+  // if (form.値段 !== null && isNaN(Number(form.値段))) {
+  //   return "値段は数値で入力してください";
+  // }
   if (!form.セット名.trim()) {
     return "セット名を選択してください";
   }
